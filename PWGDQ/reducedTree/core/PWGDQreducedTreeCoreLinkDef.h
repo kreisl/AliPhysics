39d18df31b22fed6e1b1b110ae398fdf84888e3d
Lukas Kreis <l.kreis@gsi.de>
2017-08-14 16:29:04 +0200

    Splitting of reducedTree module into core and aliroot interface

--- PWGDQ/reducedTree/core/PWGDQreducedTreeCoreLinkDef.h
+++ PWGDQ/reducedTree/core/PWGDQreducedTreeCoreLinkDef.h
@@ -6,21 +6,12 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class AliAnalysisTaskReducedEventProcessor+;
-#pragma link C++ class AliAnalysisTaskReducedTreeMaker+;
-#pragma link C++ class AliHistogramManager+;
-#pragma link C++ class AliMixingHandler+;
-#pragma link C++ class AliReducedAnalysisJpsi2ee+;
-#pragma link C++ class AliReducedAnalysisJpsi2eeMult+;
-#pragma link C++ class AliReducedAnalysisTaskSE+;
-#pragma link C++ class AliReducedAnalysisTest+;
 #pragma link C++ class AliReducedBaseEvent+;
 #pragma link C++ class AliReducedBaseTrackCut+;
 #pragma link C++ class AliReducedBaseTrack+;
 #pragma link C++ class AliReducedCaloClusterInfo+;
 #pragma link C++ class AliReducedEventCut+;
 #pragma link C++ class AliReducedEventInfo+;
-#pragma link C++ class AliReducedEventInputHandler+;
 #pragma link C++ class AliReducedEventPlaneInfo+;
 #pragma link C++ class AliReducedFMDInfo+;
 #pragma link C++ class AliReducedInfoCut+;
@@ -29,6 +20,5 @@
 #pragma link C++ class AliReducedTrackInfo+;
 #pragma link C++ class AliReducedVarCut+;
 #pragma link C++ class AliReducedVarManager+;
-#pragma link C++ class AliResonanceFits+;
 
 #endif
---
+++ PWGDQ/reducedTree/interface/PWGDQreducedTreeInterfaceLinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliAnalysisTaskReducedEventProcessor+;
+#pragma link C++ class AliAnalysisTaskReducedTreeMaker+;
+#pragma link C++ class AliHistogramManager+;
+#pragma link C++ class AliMixingHandler+;
+#pragma link C++ class AliReducedAnalysisJpsi2ee+;
+#pragma link C++ class AliReducedAnalysisJpsi2eeMult+;
+#pragma link C++ class AliReducedAnalysisTaskSE+;
+#pragma link C++ class AliReducedAnalysisTest+;
+#pragma link C++ class AliReducedEventInputHandler+;
+#pragma link C++ class AliResonanceFits+;
+
+#endif
